7453b6baaa929e8e9d8e38a9443b58242ebf6cb1
Debolu <obayemiadebolu16@gmail.com>
2026-08-29 18:27:45 +0100

    create requirements.txt

---
+++ test.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+driver.maximize_window()
+
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+time.sleep(5)
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+time.sleep(5)
+driver.find_element(By.ID, "login-button").click()
+
+time.sleep(5)
